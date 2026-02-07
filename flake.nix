1a449dd420bc01699a8c473a15d3bf4deda02e5e
eljamm <fedi.jamoussi@protonmail.ch>
2026-02-07 16:57:05 +0100

    pass args to standalone script

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
         gwt-bin = pkgs.callPackage ./nix/package.nix { };
         standalone = pkgs.writeShellScriptBin "gwt" ''
           ${pkgs.bashInteractive}/bin/bash -i -c \
-          "source ${gwt-bin.shWrapper} && gwt"
+          "source ${gwt-bin.shWrapper} && gwt \"$@\""
         '';
       in
       {
