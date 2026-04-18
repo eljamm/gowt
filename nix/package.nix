aa48dca320c4022fb43da195109409a607aadb38
eljamm <fedi.jamoussi@protonmail.ch>
2026-04-18 21:53:41 +0200

    refactor(nix): highlight hints for shell wrappers

--- nix/package.nix
+++ nix/package.nix
@@ -15,17 +15,16 @@ buildGoModule (finalAttrs: {
   src = lib.cleanSourceWith {
     name = "source";
     src = ../.;
+
     filter =
       name: type:
       let
         base = baseNameOf (toString name);
       in
-      (
-        (type == "directory" && base != ".wt")
-        || lib.hasSuffix ".go" base
-        || base == "go.mod"
-        || base == "go.sum"
-      );
+      (type == "directory" && base != ".wt")
+      || lib.hasSuffix ".go" base
+      || base == "go.mod"
+      || base == "go.sum";
   };
 
   vendorHash = "sha256-0nVmMIq+UocNPktI41rxmoisaObhGQloLeNzZdnG4bs=";
@@ -59,57 +58,61 @@ buildGoModule (finalAttrs: {
   '';
 
   passthru = {
-    # 2. The Smart Wrapper (Fish)
-    fishWrapper = writeText "gwt.fish" ''
-      function gwt
-          # Run the binary and capture output
-          set -l output (${finalAttrs.finalPackage}/bin/gwt $argv)
-          set -l exit_code $status
+    fishWrapper =
+      writeText "gwt.fish"
+        # fish
+        ''
+          function gwt
+              # Run the binary and capture output
+              set -l output (${finalAttrs.finalPackage}/bin/gwt $argv)
+              set -l exit_code $status
 
-          if test $exit_code -eq 0
-              # CRITICAL: Only cd if it is actually a directory
-              if test -d "$output"
-                  builtin cd "$output"
+              if test $exit_code -eq 0
+                  # CRITICAL: Only cd if it is actually a directory
+                  if test -d "$output"
+                      builtin cd "$output"
+                  else
+                      # Otherwise just print the text (Help, Version, etc)
+                      printf "%s\n" "$output"
+                  end
               else
-                  # Otherwise just print the text (Help, Version, etc)
+                  # Print errors
                   printf "%s\n" "$output"
+                  return $exit_code
               end
-          else
-              # Print errors
+          end
+
+          # Source the patched completions
+          source ${finalAttrs.finalPackage}/share/fish/vendor_completions.d/gwt.fish
+        '';
+
+    # NOTE: for zsh https://stackoverflow.com/a/74323525/8608146 is required
+    shWrapper =
+      writeText "gwt.sh"
+        # bash
+        ''
+          gwt() {
+            local output
+            output=$(${finalAttrs.finalPackage}/bin/gwt "$@")
+            local exit_code=$?
+
+            if [ $exit_code -eq 0 ]; then
+              if [ -d "$output" ]; then
+                builtin cd "$output"
+              else
+                printf "%s\n" "$output"
+              fi
+            else
               printf "%s\n" "$output"
               return $exit_code
-          end
-      end
+            fi
+          }
 
-      # Source the patched completions
-      source ${finalAttrs.finalPackage}/share/fish/vendor_completions.d/gwt.fish
-    '';
-
-    # 3. The Smart Wrapper (Bash/Zsh)
-    # NOTE: for zsh https://stackoverflow.com/a/74323525/8608146 is required
-    shWrapper = writeText "gwt.sh" ''
-      gwt() {
-        local output
-        output=$(${finalAttrs.finalPackage}/bin/gwt "$@")
-        local exit_code=$?
-
-        if [ $exit_code -eq 0 ]; then
-          if [ -d "$output" ]; then
-            builtin cd "$output"
-          else
-            printf "%s\n" "$output"
+          if [ -n "$ZSH_VERSION" ]; then
+             source ${finalAttrs.finalPackage}/share/zsh/site-functions/_gwt
+          elif [ -n "$BASH_VERSION" ]; then
+             source ${finalAttrs.finalPackage}/share/bash-completion/completions/gwt.bash
           fi
-        else
-          printf "%s\n" "$output"
-          return $exit_code
-        fi
-      }
-
-      if [ -n "$ZSH_VERSION" ]; then
-         source ${finalAttrs.finalPackage}/share/zsh/site-functions/_gwt
-      elif [ -n "$BASH_VERSION" ]; then
-         source ${finalAttrs.finalPackage}/share/bash-completion/completions/gwt.bash
-      fi
-    '';
+        '';
   };
 })
